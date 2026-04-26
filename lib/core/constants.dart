05524089782c7a596d034f11bdb177a7330e4aeb
Fady <fedo.zaher@gmail.com>
2026-04-26 11:46:12 +0300

    web rtc calling

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,6 +1,6 @@
 class AppConstants {
   static const String signalingServerUrl =
-      'https://thick-terms-hammer.loca.lt'; // Replace with your actual signaling server URL
+      'https://large-rooms-flow.loca.lt'; // Replace with your actual signaling server URL
   static const Map<String, dynamic> iceServers = {
     'iceServers': [
       {'urls': 'stun:stun.l.google.com:19302'},
